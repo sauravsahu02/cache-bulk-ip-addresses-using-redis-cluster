228d8c2b89782cc68df1c07eac26e234db809ed3
sauravsahu02 <sauravsahu02@gmail.com>
2022-12-19 19:16:32 +0530

    Add files via upload

--- src/main/scala/com/cache/redis/CacheBlackListedIPAddresses.scala
+++ src/main/scala/com/cache/redis/CacheBlackListedIPAddresses.scala
@@ -20,6 +20,14 @@ object CacheBlackListedIPAddresses {
     val hostAndPortNodes = new util.HashSet[HostAndPort]
     hostAndPortNodes.add(new HostAndPort("localhost", 7000))
     hostAndPortNodes.add(new HostAndPort("localhost", 7001))
+    // Running of 3 master nodes is MANDATORY in redis cluster.
+    // Example:% redis-cli -p 7000 cluster nodes
+    //498c75cadd638f9db8f8d0ac01f45388c89734ae ::1:7001@17001 master - 0 1671456426160 2 connected 5962-10922
+    //12b5e88b45b0ba580ffd4d835f069da2694d0512 ::1:7002@17002 master - 0 1671456425554 3 connected 11422-16383
+    //7f493f6218a98cd888e2fb4712fd987396dcc3dc ::1:7000@17000 myself,master - 0 1671456424000 7 connected 0-5961 10923-11421
+    //0e29128020ff704deadd4e0e663e4c50215b946b ::1:7004@17004 slave,fail 7f493f6218a98cd888e2fb4712fd987396dcc3dc 1671456198159 1671456195621 7 disconnected
+    //cb0ba3cbabff59d0852a5dfce27732cb07aef782 ::1:7005@17005 slave,fail 498c75cadd638f9db8f8d0ac01f45388c89734ae 1671456198159 1671456195621 2 disconnected
+    //4f51bd2c900cd9536b7b44074fa6d9ae0d3f803c ::1:7003@17003 slave,fail 12b5e88b45b0ba580ffd4d835f069da2694d0512 1671456197551 1671456195621 3 disconnected
     new JedisCluster(hostAndPortNodes)
   }
 }
--- src/test/scala/com/cache/redis/CacheBlackListedIPAddressesTest.scala
+++ src/test/scala/com/cache/redis/CacheBlackListedIPAddressesTest.scala
@@ -1,16 +1,20 @@
 package com.cache.redis
 import org.scalatest.FunSuite
+import redis.clients.jedis.JedisCluster
 
 import java.util
 
 class CacheBlackListedIPAddressesTest extends FunSuite {
-  val jedis = CacheBlackListedIPAddresses.getJedisCluster
+  val jedis: JedisCluster = CacheBlackListedIPAddresses.getJedisCluster
   test("Add IP to redis set and check its existence - using sadd and sismember") {
     val IPAddress01 = "197.12.12.3"
     val IPAddress02 = "197.12.12.10"
     jedis.set("ip", IPAddress01)
+    assert(jedis.get("ip") == IPAddress01)
+
     jedis.sadd("ipAddress_blocked", IPAddress01)
     jedis.sadd("ipAddress_blocked", IPAddress02)
+
     assert(jedis.sismember("ipAddress_blocked", IPAddress01))
     assert(jedis.sismember("ipAddress_blocked", IPAddress02))
     assert(!jedis.sismember("ipAddress_blocked", IPAddress02 + "0000"))
@@ -19,6 +23,7 @@ class CacheBlackListedIPAddressesTest extends FunSuite {
     jedis.set("redis", "remote data structure server")
     jedis.set("year", "2022")
     jedis.incrBy("year", 1)
+    assert(jedis.exists("redis"))
     assert(jedis.get("redis").equals("remote data structure server"))
     assert(jedis.get("year") == "2023")
   }
@@ -32,5 +37,18 @@ class CacheBlackListedIPAddressesTest extends FunSuite {
     assert(jedis.hexists("employee01", "lastName"))
     assert(!jedis.hexists("employee01", "age"))
     assert(jedis.hgetAll("employee01").size() == 2)
+  }
+  test ("Redis data types check - Lists") {
+    // List as FIFO
+    jedis.lpush("work:queue:ids", "101")
+    jedis.lpush("work:queue:ids", "102")
+    jedis.lrange("work:queue:ids", 0, -1) //Array("101", "102"))
+    assert(jedis.rpop("work:queue:ids") == "101")
+    assert(jedis.rpop("work:queue:ids") == "102")
+    // List as LIFO
+    jedis.lpush("work:queue:ids", "101")
+    jedis.lpush("work:queue:ids", "102")
+    assert(jedis.lpop("work:queue:ids") == "102")
+    assert(jedis.lpop("work:queue:ids") == "101")
   }
 }
